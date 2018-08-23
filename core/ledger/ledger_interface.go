421eae1a3c411204f5c30f0738f17461e1b9a018
肖准 <zhun.xiao@midea.com>
2018-08-23 11:31:18 +0800

    gofmt bug fix

--- core/ledger/ledger_interface.go
+++ core/ledger/ledger_interface.go
@@ -133,8 +133,7 @@ type HistoryQueryExecutor interface {
 	// The returned ResultsIterator contains results of type *KeyModification which is defined in protos/ledger/queryresult.
 	GetHistoryForKey(namespace string, key string) (commonledger.ResultsIterator, error)
 
-	GetHistoryForKeyByPage(namespace string, key string, currentPage int64, pageSize int64)(commonledger.ResultsIterator, error)
-
+	GetHistoryForKeyByPage(namespace string, key string, currentPage int64, pageSize int64) (commonledger.ResultsIterator, error)
 }
 
 // TxSimulator simulates a transaction on a consistent snapshot of the 'as recent state as possible'
